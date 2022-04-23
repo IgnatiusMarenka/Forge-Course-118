21fb032a5e8a66210ac9be7cdfd873be5e2f367f
ingo <ingomarechal@gmail.com>
2022-04-23 16:28:00 +0200

    73. Events Basics

--- src/main/java/com/superingo/mccourse/event/ModEvents.java
+++ src/main/java/com/superingo/mccourse/event/ModEvents.java
@@ -3,7 +3,10 @@ package com.superingo.mccourse.event;
 import com.superingo.mccourse.MCCourseMod;
 import com.superingo.mccourse.command.ReturnHomeCommand;
 import com.superingo.mccourse.command.SetHomeCommand;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -24,6 +27,19 @@ public class ModEvents {
         if(!event.getOriginal().getLevel().isClientSide()) {
             event.getPlayer().getPersistentData().putIntArray(MCCourseMod.MOD_ID + "homepos",
                     event.getOriginal().getPersistentData().getIntArray(MCCourseMod.MOD_ID + "homepos"));
+        }
+    }
+
+    @SubscribeEvent
+    public static void setEntityOnFireWhenHit(LivingDamageEvent event) {
+        if(!event.getEntity().level.isClientSide()) {
+            if(event.getSource().getDirectEntity() instanceof Player) {
+                Player player = (Player)event.getSource().getDirectEntity();
+                if(player.getMainHandItem().getItem() == Items.NETHER_BRICK) {
+                    player.getMainHandItem().shrink(1);
+                    event.getEntityLiving().setSecondsOnFire(2);
+                }
+            }
         }
     }
 }
