471f3f78568a7db0aa510930a003dbd2de988f54
ingo <ingomarechal@gmail.com>
2022-04-27 12:41:05 +0200

    86. Adding a Custom Menu Screen

--- src/main/java/com/superingo/mccourse/event/ModEvents.java
+++ src/main/java/com/superingo/mccourse/event/ModEvents.java
@@ -3,8 +3,13 @@ package com.superingo.mccourse.event;
 import com.superingo.mccourse.MCCourseMod;
 import com.superingo.mccourse.command.ReturnHomeCommand;
 import com.superingo.mccourse.command.SetHomeCommand;
+import com.superingo.mccourse.util.KaupenTitleScreen;
+import net.minecraft.client.gui.screens.TitleScreen;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Items;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.ScreenOpenEvent;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
@@ -40,6 +45,14 @@ public class ModEvents {
                     event.getEntityLiving().setSecondsOnFire(2);
                 }
             }
+        }
+    }
+
+    @SubscribeEvent
+    @OnlyIn(Dist.CLIENT)
+    public static void openGui(ScreenOpenEvent event) {
+        if (event.getScreen() instanceof TitleScreen && !(event.getScreen() instanceof KaupenTitleScreen)) {
+            event.setScreen(new KaupenTitleScreen());
         }
     }
 }
